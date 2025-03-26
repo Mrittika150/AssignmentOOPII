30dd33911bfa4a2d7beea5436a29da5ecda5f9d4
Mriitika150 <mrittikajahan@iut-dhaka.edu>
2025-03-26 11:38:22 +0600

    Extracting customerFinder class

---
+++ src/CustomerFinder.java
@@ -0,0 +1,23 @@
+// New file: CustomerFinder.java
+import java.util.List;
+
+public class CustomerFinder {
+
+    public Customer findById(String id, List<Customer> customers) {
+        for (Customer customer : customers) {
+            if (customer.getUserID().equals(id)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public boolean emailExists(String email, List<Customer> customers) {
+        for (Customer customer : customers) {
+            if (customer.getEmail().equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
